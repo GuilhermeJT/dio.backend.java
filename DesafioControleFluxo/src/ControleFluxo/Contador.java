71318b0a2567a81a02a921d3b1756335379734d1
GuilhermeJT <guilhermej.2309@gmail.com>
2025-07-31 15:59:01 -0400

    Exer de Controle de Fluxo finalizado

--- DesafioControleFluxo/src/ControleFluxo/Contador.java
+++ DesafioControleFluxo/src/ControleFluxo/Contador.java
@@ -1,3 +1,5 @@
+package ControleFluxo;
+
 import java.util.Scanner;
 
 public class Contador {
@@ -8,25 +10,28 @@ public class Contador {
         System.out.println("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
 
+        terminal.close();
+
         try {
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
         }catch (ParametrosInvalidosException e) {
             //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println(e.getMessage());
 
         }
 
     }
-    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException  {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if(parametroUm > parametroDois){
-            throw new ParametrosInvalidosException("Parametro 1 maior q o parametro 2");
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
         }
 
         int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
-        for(int i = 0; i < contagem; i++){
+        for(int i = 1; i <= contagem; i++){
             System.out.println("Imprimindo o número "+ i);
         }
     }
