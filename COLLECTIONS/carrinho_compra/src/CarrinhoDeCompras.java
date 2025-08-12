7535e75c986e4b653a2d828443ef950c2a98dc8b
GuilhermeJT <guilhermej.2309@gmail.com>
2025-08-12 17:22:41 -0400

    resolvendo exercicios de colletion

---
+++ COLLECTIONS/carrinho_compra/src/CarrinhoDeCompras.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> carinhoCompra;
+
+    public CarrinhoDeCompras(){
+        this.carinhoCompra = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        carinhoCompra.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensRemover = new ArrayList<>();
+
+        if(!carinhoCompra.isEmpty()){
+            for(Item t : carinhoCompra){
+                if(t.getNome().equalsIgnoreCase(nome)){
+                    itensRemover.add(t);
+                }
+            }
+            carinhoCompra.removeAll(itensRemover);
+        }
+        else{
+            System.out.println("Lista está vazia!");
+        }
+    }
+
+
+
+}
