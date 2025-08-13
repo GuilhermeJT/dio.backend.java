df1bf8ac40b1d69d71c8cba65e2862e2c86e9cb1
GuilhermeJT <guilhermej.2309@gmail.com>
2025-08-13 10:11:26 -0400

    Finalizando desafio carrinho de mercado usando List

--- COLLECTIONS/carrinho_compra/src/CarrinhoDeCompras.java
+++ COLLECTIONS/carrinho_compra/src/CarrinhoDeCompras.java
@@ -42,19 +42,6 @@ public class CarrinhoDeCompras {
         System.out.println(carinhoCompra);
     }
 
-    public static void main(String[] args) {
-        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
-
-        carrinhoDeCompras.adicionarItem("Arroz",10,2);
-        carrinhoDeCompras.adicionarItem("Feijão",5,1);
-
-        carrinhoDeCompras.calcularValorTotal();
-        carrinhoDeCompras.infosItem();
-
-        carrinhoDeCompras.removerItem("Arroz");
-        carrinhoDeCompras.infosItem();
-    }
-
 
 }
 
--- COLLECTIONS/carrinho_compra/src/Main.java
+++ COLLECTIONS/carrinho_compra/src/Main.java
@@ -1,7 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
 
+        carrinhoDeCompras.adicionarItem("Arroz",10,2);
+        carrinhoDeCompras.adicionarItem("Feijão",5,1);
+
+        carrinhoDeCompras.calcularValorTotal();
+        carrinhoDeCompras.infosItem();
+
+        carrinhoDeCompras.removerItem("Arroz");
+        carrinhoDeCompras.infosItem();
     }
 }
