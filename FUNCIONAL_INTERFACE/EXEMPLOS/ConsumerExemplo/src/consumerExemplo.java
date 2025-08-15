3bc04aeb9e85e8d8f36deb71fb256aa5a9a0d9fc
GuilhermeJT <guilhermej.2309@gmail.com>
2025-08-15 17:19:04 -0400

    Exemplos Stream

---
+++ FUNCIONAL_INTERFACE/EXEMPLOS/ConsumerExemplo/src/consumerExemplo.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class consumerExemplo {
+    public static void main(String[] args) {
+        List<Integer> lista = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        lista.stream().forEach(n -> { if(n % 2 == 0){
+            System.out.println(n);}
+        });
+    }
+}
